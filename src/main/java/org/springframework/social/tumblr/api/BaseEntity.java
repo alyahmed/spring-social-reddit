6dc6cc1a23d6c258d60794eb3ab5633308bf7485
Ahmed Aly <ahmedaly@Ahmeds-MacBook-Pro.local>
2014-10-08 23:36:43 -0400

    Added BaseEntity class

---
+++ src/main/java/org/springframework/social/tumblr/api/BaseEntity.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014 ahmedaly.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.social.tumblr.api;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author ahmedaly
+ */
+public class BaseEntity {
+    
+    private Map<String, Object> properties = new HashMap<String, Object>();
+    
+    @JsonAnySetter
+    public void setProperty(String key, Object value) {
+        properties.put(key, value);
+    }
+    
+    public Map<String, Object> getProperties() {
+        return properties;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "properties=" + properties +
+                '}';
+    }
+}
