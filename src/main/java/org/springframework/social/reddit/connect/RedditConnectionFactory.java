f6fe2f26f165dca474e71d3970a0be42b16e0ff6
Ahmed Aly <ahmedaly@Ahmeds-MacBook-Pro.local>
2014-10-22 05:46:53 -0400

    RestTemplate Configurations

--- src/main/java/org/springframework/social/reddit/connect/RedditConnectionFactory.java
+++ src/main/java/org/springframework/social/reddit/connect/RedditConnectionFactory.java
@@ -5,32 +5,27 @@
  */
 package org.springframework.social.reddit.connect;
 
-import java.util.Date;
-import org.springframework.social.connect.Connection;
-import org.springframework.social.connect.ConnectionData;
 import org.springframework.social.connect.UserProfile;
 import org.springframework.social.connect.support.OAuth2ConnectionFactory;
 import org.springframework.social.oauth2.AccessGrant;
+import org.springframework.social.oauth2.OAuth2Operations;
 import org.springframework.social.reddit.api.Reddit;
 
 /**
  *
  * @author ahmedaly
  */
-public class RedditConnectionFactory extends OAuth2ConnectionFactory<Reddit>{
+public class RedditConnectionFactory extends OAuth2ConnectionFactory<Reddit> {
 
     public RedditConnectionFactory(String clientId, String clientSecret) {
         super("reddit", new RedditServiceProvider(clientId, clientSecret), new RedditAdapter());
     }
-    
+
     @Override
-	protected String extractProviderUserId(AccessGrant accessGrant) {
-		Reddit api = ((RedditServiceProvider)getServiceProvider()).getApi(accessGrant.getAccessToken());
-	    UserProfile userProfile = getApiAdapter().fetchUserProfile(api);
-	    return userProfile.getUsername();
-	}
-    
-    
-    
-    
+    protected String extractProviderUserId(AccessGrant accessGrant) {
+        Reddit api = ((RedditServiceProvider) getServiceProvider()).getApi(accessGrant.getAccessToken());
+        UserProfile userProfile = getApiAdapter().fetchUserProfile(api);
+        return userProfile.getUsername();
+    }
+
 }
--- src/main/java/org/springframework/social/reddit/connect/RedditOAuth2Template.java
+++ src/main/java/org/springframework/social/reddit/connect/RedditOAuth2Template.java
@@ -5,6 +5,8 @@
  */
 package org.springframework.social.reddit.connect;
 
+import java.nio.charset.Charset;
+import java.util.Base64;
 import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -26,20 +28,40 @@ public class RedditOAuth2Template extends OAuth2Template {
 
     private static final Logger LOG = LogManager.getLogger(RedditOAuth2Template.class);
 
+    private String clientId;
+    private String clientSecret;
+
     public RedditOAuth2Template(String clientId, String clientSecret) {
         super(clientId, clientSecret, RedditPaths.OAUTH_AUTH_URL, RedditPaths.OAUTH_TOKEN_URL);
-        setUseParametersForClientAuthentication(true);
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
     }
 
     @Override
     @SuppressWarnings({"unchecked", "rawtypes"})
     protected AccessGrant postForAccessGrant(String accessTokenUrl, MultiValueMap<String, String> parameters) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpHeaders headers = createHeaders(clientId, clientSecret);
+        headers.set(accessTokenUrl, accessTokenUrl);
         HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(parameters, headers);
+        LOG.info("requestEntity: " + requestEntity.toString());
         ResponseEntity<Map> responseEntity = getRestTemplate().exchange(accessTokenUrl, HttpMethod.POST, requestEntity, Map.class);
+        LOG.info("response Entity: " + responseEntity);
         Map<String, Object> responseMap = responseEntity.getBody();
+        LOG.info("response map: " + responseMap);
         return extractAccessGrant(responseMap);
+    }
+
+    /*
+     Reddit requires client_id and client_secret be 
+     placed via HTTP basic Auth when retrieving the access_token
+     */
+    private HttpHeaders createHeaders(String username, String password) {
+        String auth = username + ":" + password;
+        byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(Charset.forName("US-ASCII")));
+        HttpHeaders headers = new HttpHeaders();
+        String authHeader = "Basic " + new String(encodedAuth);
+        headers.set("Authorization", authHeader);
+        return headers;
     }
 
     private AccessGrant extractAccessGrant(Map<String, Object> result) {
